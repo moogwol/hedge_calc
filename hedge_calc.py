f0e01cf1d23bdccc8cb7a4347fe3155640fed9a2
Miguel Corkhill <moogwol@msn.com>
2019-10-29 15:14:35 +0100

    linked program logic to GUI

--- hedge_calc.py
+++ hedge_calc.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QVBoxLayout,
                              QLineEdit, QPushButton)
 import sys
+from .maths import calculate_hedge_to_place
 
 class GuiWidget(QWidget):
 
@@ -12,26 +13,45 @@ class GuiWidget(QWidget):
         self.setLayout(layout)
 
         welcome_label = QLabel('Welcome to Cath\'s <h1>Hedge Calculator</h1>')
+        instruction_label = QLabel('Enter the parameters below...')
+
         layout.addWidget(welcome_label)
+        layout.addWidget(instruction_label)
 
         # Allows the user to input the hedge required
-        hedge_required = QLineEdit()
-        hedge_required.setPlaceholderText('Hedge Required')
-        layout.addWidget(hedge_required)
+        self.hedge_required = QLineEdit()
+        self.hedge_required.setPlaceholderText('Hedge Required')
+        layout.addWidget(self.hedge_required)
 
         # Allows the user to input the existing hedge
-        existing_hedge = QLineEdit()
-        existing_hedge.setPlaceholderText('Existing hedge')
-        layout.addWidget(existing_hedge)
+        self.existing_hedge = QLineEdit()
+        self.existing_hedge.setPlaceholderText('Existing hedge')
+        layout.addWidget(self.existing_hedge)
 
         # Allows the user to input the VIF
-        VIF = QLineEdit()
-        VIF.setPlaceholderText('VIF')
-        layout.addWidget(VIF)
+        self.VIF = QLineEdit()
+        self.VIF.setPlaceholderText('VIF')
+        layout.addWidget(self.VIF)
 
         # A button to get the hedge_to_place
         button = QPushButton('Calculate Hedge')
+        button.clicked.connect(self.calculate_hedge)
         layout.addWidget(button)
+
+        # A label which shows the hedge to place
+        self.hedge_to_place = QLabel("")
+        layout.addWidget(self.hedge_to_place)
+
+
+    def calculate_hedge(self):
+        hr = int(self.hedge_required.text())
+        eh = int(self.existing_hedge.text())
+        vif = int(self.VIF.text())
+        tol = 250000
+        htp = calculate_hedge_to_place(hr, eh, vif, tol)
+
+
+        self.hedge_to_place.setText(str(htp))
 
 
 
